f10351ace317c3a02368c33388619d78e5e9b258
SkyFutu <119418823+SkyFutu@users.noreply.github.com>
2025-04-15 19:29:34 +0800

    Update anthropicmsgs.py

--- pkg/provider/modelmgr/requesters/anthropicmsgs.py
+++ pkg/provider/modelmgr/requesters/anthropicmsgs.py
@@ -11,6 +11,8 @@ import anthropic
 import httpx
 
 from .. import entities, errors, requester
+
+from .. import entities, errors
 from ....core import entities as core_entities
 from ... import entities as llm_entities
 from ...tools import entities as tools_entities
@@ -32,6 +34,7 @@ class AnthropicMessages(requester.LLMAPIRequester):
 
         httpx_client = anthropic._base_client.AsyncHttpxClientWrapper(
             base_url=self.ap.provider_cfg.data['requester']['anthropic-messages']['base-url'].replace(' ', ''),
+            # cast to a valid type because mypy doesn't understand our type narrowing
             timeout=typing.cast(httpx.Timeout, self.ap.provider_cfg.data['requester']['anthropic-messages']['timeout']),
             limits=anthropic._constants.DEFAULT_CONNECTION_LIMITS,
             follow_redirects=True,
@@ -55,23 +58,30 @@ class AnthropicMessages(requester.LLMAPIRequester):
         args = self.ap.provider_cfg.data['requester']['anthropic-messages']['args'].copy()
         args["model"] = model.name if model.model_name is None else model.model_name
 
-        # 处理 system role 消息
+        # 处理消息
+
+        # system
         system_role_message = None
+
         for i, m in enumerate(messages):
             if m.role == "system":
                 system_role_message = m
+
                 break
 
         if system_role_message:
             messages.pop(i)
-            if isinstance(system_role_message.content, str):
-                args['system'] = system_role_message.content
+
+        if isinstance(system_role_message, llm_entities.Message) \
+            and isinstance(system_role_message.content, str):
+            args['system'] = system_role_message.content
 
         req_messages = []
 
         for m in messages:
             if m.role == 'tool':
                 tool_call_id = m.tool_call_id
+
                 req_messages.append({
                     "role": "user",
                     "content": [
@@ -82,23 +92,26 @@ class AnthropicMessages(requester.LLMAPIRequester):
                         }
                     ]
                 })
+
                 continue
 
             msg_dict = m.dict(exclude_none=True)
 
-            if isinstance(m.content, str) and m.content.strip():
+            if isinstance(m.content, str) and m.content.strip() != "":
                 msg_dict["content"] = [
                     {
                         "type": "text",
                         "text": m.content
                     }
                 ]
-
             elif isinstance(m.content, list):
+
                 for i, ce in enumerate(m.content):
+
                     if ce.type == "image_base64":
                         image_b64, image_format = await image.extract_b64_and_format(ce.image_base64)
-                        msg_dict["content"][i] = {
+
+                        alter_image_ele = {
                             "type": "image",
                             "source": {
                                 "type": "base64",
@@ -106,8 +119,10 @@ class AnthropicMessages(requester.LLMAPIRequester):
                                 "data": image_b64
                             }
                         }
+                        msg_dict["content"][i] = alter_image_ele
 
             if m.tool_calls:
+
                 for tool_call in m.tool_calls:
                     msg_dict["content"].append({
                         "type": "tool_use",
@@ -115,34 +130,38 @@ class AnthropicMessages(requester.LLMAPIRequester):
                         "name": tool_call.function.name,
                         "input": json.loads(tool_call.function.arguments)
                     })
+
                 del msg_dict["tool_calls"]
 
             req_messages.append(msg_dict)
+                
 
         args["messages"] = req_messages
-
+        
         if funcs:
             tools = await self.ap.tool_mgr.generate_tools_for_anthropic(funcs)
+
             if tools:
                 args["tools"] = tools
 
         try:
+            # print(json.dumps(args, indent=4, ensure_ascii=False))
             resp = await self.client.messages.create(**args)
 
-            assert isinstance(resp, anthropic.types.message.Message)
-
-            result = {
+            args = {
                 'content': '',
                 'role': resp.role,
             }
+            
+            assert type(resp) is anthropic.types.message.Message
 
             for block in resp.content:
                 if block.type == 'thinking':
-                    result['content'] = '<think>' + block.thinking + '</think>\n' + result['content']
+                    args['content'] = '<think>' + block.thinking + '</think>\n' + args['content']
                 elif block.type == 'text':
-                    result['content'] += block.text
+                    args['content'] += block.text
                 elif block.type == 'tool_use':
-                    assert isinstance(block, anthropic.types.tool_use_block.ToolUseBlock)
+                    assert type(block) is anthropic.types.tool_use_block.ToolUseBlock
                     tool_call = llm_entities.ToolCall(
                         id=block.id,
                         type="function",
@@ -151,12 +170,11 @@ class AnthropicMessages(requester.LLMAPIRequester):
                             arguments=json.dumps(block.input)
                         )
                     )
-                    if 'tool_calls' not in result:
-                        result['tool_calls'] = []
-                    result['tool_calls'].append(tool_call)
+                    if 'tool_calls' not in args:
+                        args['tool_calls'] = []
+                    args['tool_calls'].append(tool_call)
 
-            return llm_entities.Message(**result)
-
+            return llm_entities.Message(**args)
         except anthropic.AuthenticationError as e:
             raise errors.RequesterError(f'api-key 无效: {e.message}')
         except anthropic.BadRequestError as e:
