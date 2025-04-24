66f0afc7269f5e940a2ab650dd40d0def8736114
chris <1637083533@qq.com>
2025-04-24 16:32:15 +0800

    重构AboutDialog组件，简化结构并使用computed属性获取版本号

--- web/src/components/AboutDialog.vue
+++ web/src/components/AboutDialog.vue
@@ -1,47 +1,54 @@
 <template>
     <v-card class="about-dialog">
 
-        <div id="content-container">
-            <div id="logo-container">
-                <v-img id="logo-img" src="@/assets/langbot-logo.png" width="64" />
+        <div class="d-flex flex-column align-center pa-4">
+            <v-img class="about-logo" src="@/assets/langbot-logo.png" width="64" alt="LangBot Logo" />
 
-                <div id="text-container">
-                    <div id="title">
-                        LangBot
-                    </div>
+            <div class="text-center">
+                <div class="about-title mt-2">
+                    LangBot
+                </div>
 
-                    <div id="subtitle">
-                        版本 {{ proxy.$store.state.version }}
-                    </div>
+                <div class="about-subtitle mt-1">
+                    版本 {{ appVersion }}
+                </div>
 
-                    <div id="copyright">
-                        版权所有 © 2024 RockChinQ
-                    </div>
+                <div class="about-copyright mt-1 mb-2">
+                    版权所有 © 2024 RockChinQ
                 </div>
             </div>
         </div>
 
         <template v-slot:actions>
-            <v-btn id="open-source-btn" text="代码仓库" prepend-icon="mdi-github"
-                @click="openSource"></v-btn>
-            <v-btn class="ml-auto" text="关闭" prepend-icon="mdi-close" @click="close"></v-btn>
+            <v-btn class="about-action-btn" text="代码仓库" prepend-icon="mdi-github"
+                href="https://github.com/RockChinQ/LangBot" target="_blank" rel="noopener noreferrer"></v-btn>
+            <v-spacer></v-spacer>
+            <v-btn class="about-action-btn" text="关闭" prepend-icon="mdi-close" @click="close"></v-btn>
         </template>
     </v-card>
 </template>
 
 <script setup>
-import { getCurrentInstance } from 'vue'
+import { computed, getCurrentInstance } from 'vue'
 
 const { proxy } = getCurrentInstance()
+
+// 使用 computed 属性来获取版本号
+const appVersion = computed(() => {
+    try {
+        return proxy?.$store?.state?.version ?? '未知';
+    } catch (error) {
+        if (process.env.NODE_ENV === 'development') {
+            console.error('获取 Store 版本号失败:', error);
+        }
+        return '未知';
+    }
+})
 
 const emit = defineEmits(['close'])
 
 const close = () => {
     emit('close')
-}
-
-const openSource = () => {
-    window.open('https://github.com/RockChinQ/LangBot', '_blank')
 }
 </script>
 
@@ -50,41 +57,18 @@ const openSource = () => {
     padding-top: 2rem;
 }
 
-#content-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-#logo-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-#logo-img {}
-
-#text-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-
-#title {
+.about-title {
     font-size: 1.2rem;
-    margin-top: 0.5rem;
+    font-weight: 500;
 }
 
-#subtitle {
-    font-size: 0.6rem;
-    color: #3c3c3c;
-    margin-top: 0.2rem;
+.about-subtitle,
+.about-copyright {
+    font-size: 0.8rem;
+    color: rgba(0, 0, 0, 0.6);
 }
 
-#copyright {
-    font-size: 0.6rem;
-    color: #3c3c3c;
-    margin-top: 0.2rem;
-    margin-bottom: 0.5rem;
+.about-action-btn {
+    text-transform: none;
 }
 </style>
