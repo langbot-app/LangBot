fb14b1e2e186a3a0e7306adfd83580c52f2fd5d3
Junyan Qin <rockchinq@gmail.com>
2025-05-19 23:07:54 +0800

    fix: linter error

--- web/src/app/home/models/component/llm-form/LLMForm.tsx
+++ web/src/app/home/models/component/llm-form/LLMForm.tsx
@@ -329,7 +329,7 @@ export default function LLMForm({
         console.log(res);
         toast.success(t('models.testSuccess'));
       })
-      .catch((err) => {
+      .catch(() => {
         toast.error(t('models.testError'));
       })
       .finally(() => {
