6ba1339221367d53ca0f600e8cb63ef26a3125ce
Junyan Qin <rockchinq@gmail.com>
2025-06-08 15:31:59 +0800

    fix: webchat was listed on adapter list

--- pkg/platform/botmgr.py
+++ pkg/platform/botmgr.py
@@ -248,7 +248,9 @@ class PlatformManager:
                 return
 
     def get_available_adapters_info(self) -> list[dict]:
-        return [component.to_plain_dict() for component in self.adapter_components]
+        return [
+            component.to_plain_dict() for component in self.adapter_components if component.metadata.name != 'webchat'
+        ]
 
     def get_available_adapter_info_by_name(self, name: str) -> dict | None:
         for component in self.adapter_components:
