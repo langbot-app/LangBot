9d54ac0a0186f30e02ff32049ce9ae909e8217f0
fdc <2213070223@qq.com>
2025-09-15 10:18:36 +0000

    fix:line adapter  Plugin system

--- pkg/platform/sources/line.py
+++ pkg/platform/sources/line.py
@@ -14,11 +14,11 @@ import hashlib
 from Crypto.Cipher import AES
 
 
-from .. import adapter
 from ...core import app
-from ..types import message as platform_message
-from ..types import events as platform_events
-from ..types import entities as platform_entities
+import langbot_plugin.api.definition.abstract.platform.adapter as abstract_platform_adapter
+import langbot_plugin.api.entities.builtin.platform.message as platform_message
+import langbot_plugin.api.entities.builtin.platform.events as platform_events
+import langbot_plugin.api.entities.builtin.platform.entities as platform_entities
 from ..logger import EventLogger
 
 
@@ -54,7 +54,7 @@ from linebot.v3.messaging import MessagingApiBlob
 
 
 
-class LINEMessageConverter(adapter.MessageConverter):
+class LINEMessageConverter(abstract_platform_adapter.AbstractMessageConverter):
     @staticmethod
     async def yiri2target(
         message_chain: platform_message.MessageChain, api_client: ApiClient
@@ -104,7 +104,7 @@ class LINEMessageConverter(adapter.MessageConverter):
         return platform_message.MessageChain(lb_msg_list)
 
 
-class LINEEventConverter(adapter.EventConverter):
+class LINEEventConverter(abstract_platform_adapter.AbstractEventConverter):
     @staticmethod
     async def yiri2target(
         event: platform_events.MessageEvent,
@@ -150,8 +150,8 @@ class LINEEventConverter(adapter.EventConverter):
                 source_platform_object=event,
             )
 
-class LINEAdapter(adapter.MessagePlatformAdapter):
-    bot: WebhookHandler
+class LINEAdapter(abstract_platform_adapter.AbstractMessagePlatformAdapter):
+    bot: MessagingApi
     api_client: ApiClient
 
     bot_account_id: str  # 用于在流水线中识别at是否是本bot，直接以bot_name作为标识
@@ -160,45 +160,49 @@ class LINEAdapter(adapter.MessagePlatformAdapter):
 
     listeners: typing.Dict[
         typing.Type[platform_events.Event],
-        typing.Callable[[platform_events.Event, adapter.MessagePlatformAdapter], None],
+        typing.Callable[[platform_events.Event, abstract_platform_adapter.AbstractMessagePlatformAdapter], None],
     ]
 
     config: dict
     quart_app: quart.Quart
-    ap: app.Application
 
 
     card_id_dict: dict[str, str]  # 消息id到卡片id的映射，便于创建卡片后的发送消息到指定卡片
 
     seq: int  # 用于在发送卡片消息中识别消息顺序，直接以seq作为标识
 
-    def __init__(self, config: dict, ap: app.Application, logger: EventLogger):
-        self.config = config
-        self.ap = ap
-        self.logger = logger
-        self.quart_app = quart.Quart(__name__)
-        self.listeners = {}
-        self.card_id_dict = {}
-        self.seq = 1
+    def __init__(self, config: dict, logger: EventLogger):
+        configuration = Configuration(access_token=config['channel_access_token'])
+        line_webhook = WebhookHandler(config['channel_secret'])
+        parser = WebhookParser(config['channel_secret'])
+        api_client = ApiClient(configuration)
 
-        self.event_converter = LINEEventConverter()
-        self.message_converter = LINEMessageConverter()
+        bot_account_id = config.get('bot_account_id', 'langbot')
 
-        self.configuration = Configuration(access_token=config['channel_access_token'])
-        self.line_webhook = WebhookHandler(config['channel_secret'])
-        self.parser = WebhookParser(config['channel_secret'])
-        self.api_client = ApiClient(self.configuration)
-        self.bot = MessagingApi(self.api_client)
+
+        super().__init__(
+            config = config,
+            logger = logger,
+            quart_app = quart.Quart(__name__),
+            listeners = {},
+            card_id_dict = {},
+            seq = 1,
+            event_converter = LINEEventConverter(),
+            message_converter = LINEMessageConverter(),
+            line_webhook = line_webhook,
+            parser = parser,
+            configuration=configuration,
+            api_client = api_client,
+            bot = MessagingApi(api_client),
+            bot_account_id = bot_account_id,
+        )
 
         @self.quart_app.route('/line/callback', methods=['POST'])
         async def line_callback():
             try:
                 signature = quart.request.headers.get('X-Line-Signature')
                 body = await quart.request.get_data(as_text=True)
-                events = self.parser.parse(body, signature)  # 解密解析消息
-
-                # print(body.events)
-                # self.logger.info("Request body: " + body)
+                events = parser.parse(body, signature)  # 解密解析消息
 
                 try:
 
@@ -215,6 +219,8 @@ class LINEAdapter(adapter.MessagePlatformAdapter):
             except Exception:
                 await self.logger.error(f'Error in LINE callback: {traceback.format_exc()}')
                 return {'code': 500, 'message': 'error'}
+        
+        
 
 
     
@@ -253,14 +259,14 @@ class LINEAdapter(adapter.MessagePlatformAdapter):
     def register_listener(
         self,
         event_type: typing.Type[platform_events.Event],
-        callback: typing.Callable[[platform_events.Event, adapter.MessagePlatformAdapter], None],
+        callback: typing.Callable[[platform_events.Event, abstract_platform_adapter.AbstractMessagePlatformAdapter], None],
     ):
         self.listeners[event_type] = callback
 
     def unregister_listener(
         self,
         event_type: typing.Type[platform_events.Event],
-        callback: typing.Callable[[platform_events.Event, adapter.MessagePlatformAdapter], None],
+        callback: typing.Callable[[platform_events.Event, abstract_platform_adapter.AbstractMessagePlatformAdapter], None],
     ):
         self.listeners.pop(event_type)
 
