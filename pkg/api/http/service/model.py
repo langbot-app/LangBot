60a793f25eb1ca0f2af953f0c1f0d8c1d0f16853
Junyan Qin <rockchinq@gmail.com>
2025-11-05 15:49:31 +0800

    perf: llm model testing prompt

--- pkg/api/http/service/model.py
+++ pkg/api/http/service/model.py
@@ -108,12 +108,12 @@ class LLMModelsService:
         # 有些模型厂商默认开启了思考功能，测试容易延迟
         extra_args = model_data.get('extra_args', {})
         if not extra_args or 'thinking' not in extra_args:
-            extra_args['thinking'] = {"type": "disabled"}
+            extra_args['thinking'] = {'type': 'disabled'}
 
         await runtime_llm_model.requester.invoke_llm(
             query=None,
             model=runtime_llm_model,
-            messages=[provider_message.Message(role='user', content='Hello, world!')],
+            messages=[provider_message.Message(role='user', content='Hello, world! Please just reply a "Hello".')],
             funcs=[],
             extra_args=extra_args,
         )
