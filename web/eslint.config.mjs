335f7b60c824f559a985f2560f219386685e9e78
Typer_Body <marcelacelani74@gmail.com>
2026-03-01 21:54:27 +0800

    Update eslint.config.mjs

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -11,9 +11,6 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  {
-    ignores: ['.next/**', 'node_modules/**'],
-  },
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   eslintPluginPrettierRecommended,
 ];
