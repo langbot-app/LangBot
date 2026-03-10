4545bae1edfc89c42c262b25242361877b6556a4
Junyan Qin <rockchinq@gmail.com>
2026-03-10 22:00:33 +0800

    fix: update required database version to 21

--- src/langbot/pkg/utils/constants.py
+++ src/langbot/pkg/utils/constants.py
@@ -2,11 +2,7 @@ import langbot
 
 semantic_version = f'v{langbot.__version__}'
 
-<<<<<<< stopseederror
 required_database_version = 21
-=======
-required_database_version = 20
->>>>>>> master
 """Tag the version of the database schema, used to check if the database needs to be migrated"""
 
 debug_mode = False
