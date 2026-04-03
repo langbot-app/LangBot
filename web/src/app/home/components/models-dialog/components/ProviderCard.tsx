85d281cc49051714cf70e7f041be1ca0dc729a00
sheetung <755855262@qq.com>
2026-04-03 18:29:40 +0000

    feat(models): add provider model scanning

--- web/src/app/home/components/models-dialog/components/ProviderCard.tsx
+++ web/src/app/home/components/models-dialog/components/ProviderCard.tsx
@@ -26,7 +26,14 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { useTranslation } from 'react-i18next';
 import langbotIcon from '@/app/assets/langbot-logo.webp';
-import { ExtraArg, ModelType, TestResult, ProviderModels } from '../types';
+import {
+  ExtraArg,
+  ModelType,
+  ScanModelsResult,
+  SelectedScannedModel,
+  TestResult,
+  ProviderModels,
+} from '../types';
 import ModelItem from './ModelItem';
 import AddModelPopover from './AddModelPopover';
 
@@ -54,6 +61,11 @@ interface ProviderCardProps {
     name: string,
     abilities: string[],
     extraArgs: ExtraArg[],
+  ) => Promise<void>;
+  onScanModels: (modelType: ModelType) => Promise<ScanModelsResult>;
+  onAddScannedModels: (
+    modelType: ModelType,
+    models: SelectedScannedModel[],
   ) => Promise<void>;
   onOpenEditModel: (modelId: string) => void;
   onCloseEditModel: () => void;
@@ -103,6 +115,8 @@ export default function ProviderCard({
   onOpenAddModel,
   onCloseAddModel,
   onAddModel,
+  onScanModels,
+  onAddScannedModels,
   onOpenEditModel,
   onCloseEditModel,
   onUpdateModel,
@@ -300,6 +314,8 @@ export default function ProviderCard({
                 onOpen={onOpenAddModel}
                 onClose={onCloseAddModel}
                 onAddModel={onAddModel}
+                onScanModels={onScanModels}
+                onAddScannedModels={onAddScannedModels}
                 onTestModel={onTestModel}
                 isSubmitting={isSubmitting}
                 isTesting={isTesting}
